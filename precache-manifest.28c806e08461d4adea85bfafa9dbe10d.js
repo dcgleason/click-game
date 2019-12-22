e171072b74632c32682e2737939775ffdd67e1b6
Danny Gleason <danny.c.gleason@gmail.com>
2019-12-22 12:33:31 -0500

    Updates

---
+++ precache-manifest.28c806e08461d4adea85bfafa9dbe10d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231f5902ad8846904247de19e33f9efb",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "53eda65873fcc7a57328",
+    "url": "/click-game/static/css/main.41ce3f09.chunk.css"
+  },
+  {
+    "revision": "5d32d0f65edfbdb88915",
+    "url": "/click-game/static/js/2.c3f11614.chunk.js"
+  },
+  {
+    "revision": "e92e356b467ce5268290e03394dbf217",
+    "url": "/click-game/static/js/2.c3f11614.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53eda65873fcc7a57328",
+    "url": "/click-game/static/js/main.17fce52d.chunk.js"
+  },
+  {
+    "revision": "eee0d95675b92dc4464f",
+    "url": "/click-game/static/js/runtime-main.3ba4c756.js"
+  }
+]);
